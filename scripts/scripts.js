a68ae9c592396d9a4ec997c621bc3c9d3381fd52
João Almeida <jvastos@gmail.com>
2021-09-17 00:32:05 +0200

    Big Commit before show 2
    - Random questions
    - Responsive fonts
    - Button changing

---
+++ scripts/scripts.js
@@ -0,0 +1,95 @@
+//Loader
+
+let loader = document.querySelector(".loader-wraper");
+
+window.onload = function() {
+ loader.style.display = 'none';
+ //getNewQuestion(); // initialize process to get a question from the json
+}
+
+
+document.getElementById("myBtn").addEventListener("click", function submitBreak(event){
+    event.preventDefault()
+  });
+
+
+
+        
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+    modal.style.display = "none";
+    }
+}
+
+
+
+
+// Function to get questions from the json file
+
+let questionsAlreadyDone = [];
+
+function getNewQuestion() {
+    document.getElementById("myBtn").innerHTML = "Next";
+    document.getElementById("myBtn").classList.add("myBtn--mod");
+    document.getElementById("game-title").classList.add("h1--mod");
+    fetch("../questions.json")
+    .then((response) => response.json())
+    .then((data) => {
+        if (questionsAlreadyDone.length > 5) {
+            modal.style.display = "block";
+        } else {
+        let randomQuestionIndex = Math.floor(Math.random() * data.length);
+        while (questionsAlreadyDone.includes(randomQuestionIndex)) {
+            randomQuestionIndex = Math.floor(Math.random() * data.length);
+        }
+        let label = data[randomQuestionIndex].label;
+        let question = data[randomQuestionIndex].input;
+        document.getElementById("question-title").innerHTML = label;
+        document.getElementById("question-input").innerHTML = question;
+        questionsAlreadyDone.push(randomQuestionIndex);
+        console.log(questionsAlreadyDone);
+    };
+    if (questionsAlreadyDone.length > 5) {
+        document.getElementById("question-title").innerHTML = "Rad!";
+        document.getElementById("question-title").style.fontSize = "5rem";
+        document.getElementById("question-input").innerHTML = "";
+        document.getElementById("myBtn").innerHTML = "Finish";
+    };
+})};
+
+
+
+
+
+
+/*function getQuestion() {
+    fetch("../questions.json")
+    .then((response) => response.json())
+    .then((data) => {
+        let randomQuestionIndex = Math.floor(Math.random() * data.length);
+        let label = data[randomQuestionIndex].label;
+        let question = data[randomQuestionIndex].input;
+        document.getElementById("question-title").innerHTML = label;
+        document.getElementById("question-input").innerHTML = question;
+        questionsAlreadyDone.push(randomQuestionIndex);
+        console.log(questionsAlreadyDone);
+    })
+}; */
